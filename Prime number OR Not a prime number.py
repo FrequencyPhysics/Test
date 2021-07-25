0f6d2a0e2bd943fc12d249ecbaf35de52816e9de
FrequencyPhysics <86565124+FrequencyPhysics@users.noreply.github.com>
2021-07-25 16:45:30 +0100

    Create Prime number OR Not a prime number.py

---
+++ Prime number OR Not a prime number.py
@@ -0,0 +1,16 @@
+#25/07/2021
+
+# Program to check if a number is prime or not
+num = 4567
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            print(i, "times", num // i, "is", num)
+            break
+    else:
+        print(num, "is a prime number")
+
+else:
+    print(num, "is not a prime number")
